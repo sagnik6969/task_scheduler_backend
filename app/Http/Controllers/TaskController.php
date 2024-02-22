a99ab0c9c9b1da0c52e273e3ce4e4ddf9ac30a0f
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-22 19:36:55 +0530

    fixed minor bugs

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,20 +6,15 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
@@ -27,22 +22,18 @@ class TaskController extends Controller
      */
     public function show(string $id)
     {
-        //
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
-        //
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(string $id)
     {
-        //
+
     }
 }
