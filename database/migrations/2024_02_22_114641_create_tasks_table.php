c6d88e85188412fca73e94390d9731f44d40c03f
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-23 11:02:56 +0530

    changed some migrations

--- database/migrations/2024_02_22_114641_create_tasks_table.php
+++ database/migrations/2024_02_22_114641_create_tasks_table.php
@@ -1,37 +1,38 @@
-    <?php
+<?php
 
-    use App\Models\Task;
-    use App\Models\User;
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-    return new class extends Migration {
-        /**
-         * Run the migrations.
-         */
-        public function up(): void
-        {
-            Schema::create('tasks', function (Blueprint $table) {
-                $table->id();
-                $table->string('title');
-                $table->text('description')->nullable();
-                $table->dateTime('deadline');
-                $table->boolean('is_completed')->default(0);
-                $table->unsignedTinyInteger('progress')->default(0);
-                $table->enum('priority',Task::$priorities); 
-                // $table->string('priority')
-                // $table->foreignIdFor(User::class)->nullable()->constrained()->cascadeOnDelete();
-                $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
-                $table->timestamps(); 
-            });
-        }
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('deadline');
+            $table->boolean('is_completed')->default(0);
+            $table->unsignedTinyInteger('progress')->default(0);
+            $table->enum('priority', Task::$priorities);
+            $table->unsignedBigInteger('admin_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('admin_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
 
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void
-        {
-            Schema::dropIfExists('tasks');
-        }
-    };
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
