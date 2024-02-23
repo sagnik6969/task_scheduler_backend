9245c7938abd10723ee8fe4752e5d2332acf3088
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-23 17:25:06 +0530

    added validation tests to signup route

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -29,6 +29,36 @@ class AuthTest extends TestCase
         ]);
     }
 
+    public static function invalidUserProvider()
+    {
+        $user =
+            [
+                "name" => "sagnik",
+                "email" => "userid@gmail.com",
+                "password" => "12345678",
+                "password_confirmation" => "12345678"
+            ]
+        ;
+
+        $users = [];
+
+        foreach ($user as $key => $value) {
+            $userCopy = $user;
+            $userCopy[$key] = null;
+            $users["when {$key} is empty"] = [$userCopy];
+        }
+        return $users;
+    }
+
+    /**
+     * @dataProvider invalidUserProvider
+     */
+    public function test_validation_errors_in_signup_route($user)
+    {
+        $response = $this->postJson('/api/register', $user);
+        $response->assertStatus(422);
+    }
+
 
     public function test_user_can_login()
     {
