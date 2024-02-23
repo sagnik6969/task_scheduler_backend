8a566de58580145371eb88dabcac6970af832f84
sameer <sameertrip123@example.com>
2024-02-23 09:43:32 +0530

    Added login and logout routes.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\AdminLoginNotification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->only(['logout']);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        if (Auth::attempt($data)) {
+
+            $request->session()->regenerate();
+
+            $user = auth()->user();
+
+            // $user->notify(new AdminLoginNotification());
+            return response()->json([
+                'user' => $user
+            ]);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect']
+        ]);
+    }
+
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
+}
