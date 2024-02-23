1509062cd03a406e926c3e01c83e5add5a5b5579
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-23 10:34:00 +0530

    gixed minor gugs

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -13,10 +13,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    public function tasks(): BelongsToMany
-    {
-        return $this->belongsToMany(Task::class, 'user_task');
-    }
     /**
      * The attributes that are mass assignable.
      *
@@ -47,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function task(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
