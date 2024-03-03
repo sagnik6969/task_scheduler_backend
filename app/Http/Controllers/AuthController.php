72feb2e0b744750f7e9ea817607c21bfeb61c7ff
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-03-03 12:56:28 +0530

    Refactored auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,21 +39,20 @@ class AuthController extends Controller
     {
         $id = $request->id;
         $hash = $request->hash;
-        $user = User::findOrFail($id);
+        $user = User::find($id);
 
-        if (!hash_equals(sha1($user->getEmailForVerification()), (string) $hash)) {
-            throw new AuthorizationException();
-        }
+        if (!$user)
+            return abort('user not found', 404);
+
+        if (!hash_equals(sha1($user->getEmailForVerification()), (string) $hash))
+            return abort('invalid email verification link', 422);
 
         if (!$user->hasVerifiedEmail()) {
             $user->markEmailAsVerified();
-
             return redirect(env('FRONTEND_URL', 'http://localhost:5173') . '/login?message=Email verified successfully. Please login using your registered email address and password');
 
         } else
             return abort('email already verified');
-
-        // return redirect('/verify-email/' . $id . '/' . $hash);
     }
 
     public function login(Request $request)
